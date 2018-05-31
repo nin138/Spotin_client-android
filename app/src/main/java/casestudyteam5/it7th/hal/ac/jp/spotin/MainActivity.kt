4c43e24fa658a49404c8f0e6a33d485c558e7eec
yanokunpei <ninian138@gmail.com>
2018-05-31 23:31:57 +0900

    first commit

---
+++ app/src/main/java/casestudyteam5/it7th/hal/ac/jp/spotin/MainActivity.kt
@@ -0,0 +1,12 @@
+package casestudyteam5.it7th.hal.ac.jp.spotin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
