3c93a4ebc59d635d1d102cd99c9cf8c306447229
yanokunpei <ninian138@gmail.com>
2018-06-18 12:53:58 +0900

    merge fix conflict

--- app/src/main/java/casestudyteam5/it7th/hal/ac/jp/spotin/main/MainActivity.kt
+++ app/src/main/java/casestudyteam5/it7th/hal/ac/jp/spotin/main/MainActivity.kt
@@ -1,33 +1,24 @@
 package casestudyteam5.it7th.hal.ac.jp.spotin.main
 
 import android.os.Bundle
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.OnMapReadyCallback
 import android.support.v4.app.FragmentActivity
 import casestudyteam5.it7th.hal.ac.jp.spotin.R
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import casestudyteam5.it7th.hal.ac.jp.spotin.map.MapActivity
 
-class MainActivity : FragmentActivity(), OnMapReadyCallback {
 
-  private var mMap: GoogleMap? = null
+class MainActivity : FragmentActivity() {
+
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    val mapFragment = supportFragmentManager
-      .findFragmentById(R.id.map) as SupportMapFragment
-    mapFragment.getMapAsync(this)
+    to_map.setOnClickListener {
+      val intent = Intent(this@MainActivity, MapActivity::class.java)
+      startActivity(intent)
+    }
   }
 
-  override fun onMapReady(googleMap: GoogleMap) {
-    mMap = googleMap
-
-    val tokyo = LatLng(35.681167, 139.767052)
-    mMap!!.addMarker(MarkerOptions().position(tokyo).title("Marker in TokyoStation"))
-    mMap!!.moveCamera(CameraUpdateFactory.newLatLng(tokyo))
-  }
 }
