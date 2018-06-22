961bf739caf8ba66ca9a28567601100e288c093d
muto <masa0509mutou@yahoo.co.jp>
2018-06-22 14:13:06 +0900

    [fix] データベースからのデータ取得の修正

--- app/src/main/java/casestudyteam5/it7th/hal/ac/jp/spotin/data/source/SpotStore.kt
+++ app/src/main/java/casestudyteam5/it7th/hal/ac/jp/spotin/data/source/SpotStore.kt
@@ -5,11 +5,10 @@ import android.arch.persistence.room.Relation
 import casestudyteam5.it7th.hal.ac.jp.spotin.data.TravelRecord
 
 //取得データ格納用
-class SpotStore{
+class SpotStore {
   @Embedded
   lateinit var travelRecord: TravelRecord
 
   @Relation(parentColumn = "place_id", entityColumn = "place_id")
   lateinit var spotImageList: List<TravelRecord.SpotImage>
 }
-
