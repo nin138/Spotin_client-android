54f527a9bb4c2ea6aaa043337b55763cbebcc333
yanokunpei <ninian138@gmail.com>
2018-06-23 20:52:09 +0900

    [add] di

---
+++ app/src/main/java/di/ActivityModule.kt
@@ -0,0 +1,12 @@
+package casestudyteam5.it7th.hal.ac.jp.spotin.di
+
+import casestudyteam5.it7th.hal.ac.jp.spotin.map.MapActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+interface ActivityModule {
+
+  @ContributesAndroidInjector
+  fun contributeMapActivity(): MapActivity
+}
