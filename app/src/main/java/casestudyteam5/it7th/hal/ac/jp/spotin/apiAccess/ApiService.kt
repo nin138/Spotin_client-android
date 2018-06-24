7c9eb32dfd0f0495963b7b0b76be9d694791ea4a
yoshidayoshidayoshida <m.inoru.h8@gmail.com>
2018-06-24 23:28:09 +0900

    [add] apiservice.kt

--- app/src/main/java/casestudyteam5/it7th/hal/ac/jp/spotin/apiAccess/ApiService.kt
+++ app/src/main/java/casestudyteam5/it7th/hal/ac/jp/spotin/apiAccess/ApiService.kt
@@ -1,20 +1,19 @@
 package casestudyteam5.it7th.hal.ac.jp.spotin.apiAccess
 
 import casestudyteam5.it7th.hal.ac.jp.spotin.apiAccess.Entity.JsonData
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import kotlin.jvm.java
 
-class ApiService{
-  fun getDataList(json:String, dataClass:JsonData): JsonData {
+class ApiService {
+  fun getDataList(json: String, dataClass: JsonData): JsonData {
     val moshi = Moshi.Builder().build()
     val adapter = moshi.adapter(dataClass::class.java)
     val jsonDataList = adapter.fromJson(json)
     return jsonDataList!!
   }
-  fun getJsonString(url : String): String {
+  fun getJsonString(url: String): String {
     val request = Request.Builder().url(url).build()
     val response = OkHttpClient().newCall(request).execute()
     return response.body()!!.string()
