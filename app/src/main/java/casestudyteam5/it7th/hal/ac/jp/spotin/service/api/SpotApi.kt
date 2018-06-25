862c24ac877995dc254640929642f2bf1e5f19d7
yanokunpei <ninian138@gmail.com>
2018-06-25 10:22:33 +0900

    [add] spotapi method

--- app/src/main/java/casestudyteam5/it7th/hal/ac/jp/spotin/service/api/SpotApi.kt
+++ app/src/main/java/casestudyteam5/it7th/hal/ac/jp/spotin/service/api/SpotApi.kt
@@ -1,5 +1,6 @@
 package casestudyteam5.it7th.hal.ac.jp.spotin.service.api
 
+import casestudyteam5.it7th.hal.ac.jp.spotin.service.api.entity.Spot
 import casestudyteam5.it7th.hal.ac.jp.spotin.service.api.entity.Spots
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.Deferred
@@ -16,7 +17,12 @@ class SpotApi @Inject constructor(private val service: Service) {
       @Path("lat")lat: Double,
       @Path("lng")lng: Double
     ): Deferred<Spots>
+
+    @GET("spot/details/{place_id}")
+    fun getSpotDetail(@Path("place_id")placeId: String): Deferred<Spot>
   }
+
+
 
   suspend fun getSpotList(category: String, lat: Double, lng: Double): Spots = withContext(CommonPool) {
     service.getSpotList(category, lat, lng).await()
